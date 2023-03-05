7e95a650f519f5ec33986294242a0cf1cc576cb8
KoichiDevs <alexisken1432@gmail.com>
2023-03-06 00:59:33 +0800

    removed robot

--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -37,12 +37,10 @@ const Hero = () => {
             <section className='w-full h-screen bg-hero relative text-white overflow-x-hidden z-10 overflow-hidden' ref={myRef}>
 
                 <Nav />
-                
-                <motion.img initial={{ y: 200, opacity: 0 }} animate={{ opacity: 100, y: 0 }} transition={{ duration: 1 }} alt="HeroRobot" src="/hero.webp" className='absolute -bottom-10 lg:right-0 h-full z-10 right-[-15rem] min-w-[45rem] sm:block hidden'></motion.img>
 
-                <div className='w-full max-w-[1700px] mx-auto flex items-center h-full lg:px-20 z-10 relative text-center lg:text-left justify-center lg:justify-start'>
+                <div className='w-fit mx-auto flex items-center h-full lg:px-20 z-10 relative text-center justify-center lg:justify-start '>
                     <div className=''>
-                        <motion.div initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1 }} className='font-saira lg:text-8xl font-black text-4xl 2xl:text-9xl flex items-center gap-x-2 mx-auto w-fit lg:mx-0'>
+                        <motion.div initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1 }} className='font-saira lg:text-8xl font-black text-4xl 2xl:text-9xl flex items-center gap-x-2 mx-auto w-fit'>
                             <p className="flex gap-x-[2px]">
                                 TRADER A<span className='lg:hidden block'>I</span>
                             </p>
@@ -53,7 +51,7 @@ const Hero = () => {
 
                         <motion.p initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.1 }} className='mt-2 2xl:max-w-[50rem] xl:max-w-[40rem] lg:max-w-[30rem] max-w-[40rem] font-poppins lg:text-md sm:text-sm text-xs px-10 lg:px-0 2xl:text-lg'>Trader AI project is designed to provide an easy-to-use tool for stock traders. By using artificial intelligence technology, it automates trading decisions using data analysis and predictive analytics. The AI technology continually assesses the available data and makes recommendations on when to enter or exit a trade, in order to maximize profits. It takes into account a number of factors such as risk levels, market trends and news reports, in order to make the best trading decisions. This allows cryptocurrency traders to make faster and more accurate decisions with less effort. As a result, it helps them achieve better returns on their investments.</motion.p>
 
-                        <div className='flex gap-x-3 flex-col lg:flex-row items-center justify-center lg:justify-start gap-y-5 mt-10'>
+                        <div className='flex gap-x-3 flex-col lg:flex-row items-center justify-center lg:justify-start gap-y-5 mt-10 mx-auto w-fit'>
 
                             <motion.a initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.15 }} href="" target="_blank" className=''>
                                 <Button text="Buy token" />
